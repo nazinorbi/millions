86987eb59095c628988040297fd49a473dfab299
Norbi <nazinorbi@gmail.com>
2016-10-31 20:44:22 +0100

    halad a fejlesztés

--- src/IndexBundle/Libs/Paginator.php
+++ src/IndexBundle/Libs/Paginator.php
@@ -1,124 +1,109 @@
 <?php
-    namespace Millions;
+    namespace IndexBundle\Libs;
     /**
      * PHP Pagination Class
-     * @author admin@catchmyfame.com - http://www.catchmyfame.com
+     * @auth|| admin@catchmyfame.com - http://www.catchmyfame.com
      * @version 3.0.0
      * @date February 6, 2014
      * @copyright (c) admin@catchmyfame.com (www.catchmyfame.com)
-     * @license CC Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) - http://creativecommons.org/licenses/by-sa/3.0/
+     * @license CC Attribution-ShareAlike 3.0 Unp||ted (CC BY-SA 3.0) - http://creativecommons.||g/licenses/by-sa/3.0/
      */
+    use Symfony\Component\DependencyInjection\Container;
+    use Doctrine\ORM\Query;
 
-    class Paginate
+    class Paginator
     {
-        public $current_page;
-        public $items_per_page;
-        public $limit_end;
-        public $limit_start;
-        public $num_pages;
-        public $total_items;
-        public $ipp_array;
-        protected $limit;
-        protected $mid_range;
-        protected $querystring;
-        public $return;
-        protected $get_ipp;
-        public $container;
-        private $twig;
-        private $lang;
+        private $current_page;
+        private $items_per_page;
+        private $mid_range;
+        private $ipp_array;
+        private $total_items;
+        private $defaultModelName;
         private $url;
-        private $instance;
 
-        public function setContainer($container)
+        private $limit_end;
+        private $limit_start;
+        private $num_pages;
+        private $limit;
+        private $querystring;
+
+        private $get_ipp;
+        private $start_range;
+        private $end_range;
+        private $range;
+
+        private $return;
+        protected $container;
+
+        public function __construct($config, Container $container)
         {
+            $this->createReturnObj();
+
             $this->container = $container;
+            $this->ipp_array = $config['ippArray'];
+            $this->items_per_page = $config['items_per_page'];
+            $this->mid_range = $config['midRange'];
+            $this->current_page = $config['current_page'];
+            $this->defaultModelName = $config['defaultModelName'];
         }
 
-        public function  setValue($value)
-        {
-            $this->mid_range = (int)$value->midRange;
-            $this->total_items = (int)$value->maxNumRows;
-           // $this->ipp_array = array( 3, 6, 9, 12, 25, 50, 100, 250, 'All');
-            $this->ipp_array = $value->ippArray;
-            $this->instance = $value->instance;
-            $this->current_page = $value->current_page;
-            $this->lang = $value->lang;
+        public function paginate(array $parameters, $request) {
 
-            if(isset($_GET["url"])) {
-                $this->url = $_GET["url"];
-            } elseif(isset($_POST['data']['url'])) {
-                $this->url = $_POST['data']['url'];
+            $this->url = $request->get('_route');
+            print_r($parameters);
+
+            foreach ($parameters as $key => $parameter) {
+                switch ($key) {
+                    case 'totalItems';
+                        if(is_int($parameter)) {
+                            $this->total_items = $parameter;
+                            $this->num_pages = ceil($this->total_items / $this->items_per_page);
+                        } else {
+                            $this->total_items = $this->getEntityManager()
+                                ->createQuery('
+                                    SELECT count(*) as total
+                                    FROM '.$parameters['tableName'].': tableName
+                                    WHERE tabbleName = :tableName
+                                    ')->setParameter('tableName', $parameters['tableName']);
+                            $this->num_pages = ceil($this->total_items / $this->items_per_page);
+                        }
+                    break;
+                    case 'midRange':
+                        $this->mid_range = $parameter;
+                    break;
+                    case 'ippArray':
+                        $this->ipp_array = $parameter;
+                    break;
+                    case 'current_page':
+                        $this->current_page = $parameter;
+                    break;
+                    case 'defaultModelName':
+                        $this->defaultModelName = $parameter;
+                    break;
+                }
             }
-
-            $this->getLang();
-            $this->paginate();
+            return call_user_func([$this, $this->defaultModelName]);
         }
 
-        public function RunModel()
+        public function model_1()
         {
-            $this->paginate();
-        }
-
-        public function paginate()
-        {
-            $this->twig = $this->container->get('getTwigObj')->getTwigObj();
             if($this->total_items <= 0) {
                 exit("Unable to paginate: Invalid total value (must be an integer > 0)");
             }
-            if($this->mid_range % 2 == 0 Or $this->mid_range < 1) {
+            if($this->mid_range % 2 == 0 || $this->mid_range < 1) {
                 exit("Unable to paginate: Invalid mid_range value (must be an odd integer >= 1)");
             }
             if(!is_array($this->ipp_array)) {
                 exit("Unable to paginate: Invalid ipp_array value");
             }
 
-            if(isset($_POST['ajax']) ) {
-                //$this->items_per_page = (isset($_POST["data"]['ipp'])) ? $_POST["data"]['ipp'] : $this->ipp_array[0];
-                $this->items_per_page = $this->ipp_array[0];
-                $this->default_ipp = 6;
 
-                if($this->items_per_page == "All") {
-                    $this->num_pages = 1;
-                } else {
-                    if(!is_numeric($this->items_per_page) OR $this->items_per_page <= 0) $this->items_per_page = $this->ipp_array[0];
-                    $this->num_pages = ceil($this->total_items / $this->items_per_page);
-                }
-            } else {
-                $this->items_per_page = (isset($_GET["ipp"])) ? $_GET["ipp"] : $this->ipp_array[0];
-                $this->default_ipp = $this->ipp_array[0];
-
-                if($this->items_per_page == "All") {
-                    $this->num_pages = 1;
-                } else {
-                    if(!is_numeric($this->items_per_page) OR $this->items_per_page <= 0) $this->items_per_page = $this->ipp_array[0];
-                    $this->num_pages = ceil($this->total_items / $this->items_per_page);
-                }
-
-                $this->current_page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1; // must be numeric > 0
-            }
-
-            if($_GET) {
-                array_splice($_GET, 3);
-                if(isset($_GET["PaginateToGo"])) {
-                    //  print_r($_GET["PaginateToGo"]);
-                    $args[0] = "page=" . $_GET["page"];
-                    $args[1] = "ipp=" . $_GET["ipp"];
-                    $args[2] = "url=" . $_GET['url'];
-                } else {
-                    $args = explode("&", $_SERVER["QUERY_STRING"]);
-                }
-                foreach ($args as $arg) {
-                    $keyval = explode("=", $arg);
-                    //   print_r($keyval);
-                    if($keyval[0] != "page" And $keyval[0] != "ipp") $this->querystring .= "&" . $arg;
-                }
-            }
-
-           if($this->num_pages > 10) {
-                if(($this->current_page > 1 And $this->total_items >= 10)) {
+            $this->return->num_pages = $this->num_pages;
+            if($this->num_pages > 10) {
+                if($this->current_page > 1 && $this->total_items >= 10) {
                     $this->return->prev->status = true;
+                    $this->return->prev->page = $this->current_page - 1;
                     $this->return->prev->url = $this->url;
-                    $this->return->perv->page = $this->current_page - 1;
                     $this->return->prev->ipp = $this->querystring;
                 } else {
                    $this->return->prev->status = false;
@@ -135,26 +120,26 @@
                 }
                 $this->range = range($this->start_range, $this->end_range);
                 for ($i = 1; $i <= $this->num_pages; $i++) {
-                    if($this->range[0] > 2 And $i == $this->range[0]) {
+                    $this->return->pages->$i = new \stdClass();
+                    if($this->range[0] > 2 && $i == $this->range[0]) {
                         $this->return->pages->$i->dots = true;
                     }
-                    if($i == 1 Or $i == $this->num_pages Or in_array($i, $this->range)) {
-                        if($i == $this->current_page And $this->items_per_page != "All") {
-
+                    if($i == 1 || $i == $this->num_pages || in_array($i, $this->range)) {
+                        if($i == $this->current_page && $this->items_per_page != "All") {
                             $this->return->pages->$i->title = $this->num_pages;
                             $this->return->pages->$i->i = $i;
                         } else {
                             $this->return->pages->$i->url = $this->url;
-                            $this->return->pages->$i->ipp = (int)$this->items_per_page;
+                            $this->return->pages->$i->ipp = $this->items_per_page;
                             $this->return->pages->$i->i = $i;
                             $this->return->pages->$i->title = $this->num_pages;
                         }
                     }
-                    if($this->range[ $this->mid_range - 1 ] < $this->num_pages - 1 And $i == $this->range[ $this->mid_range - 1 ]) {
-                        $this->return->page->$i->dots = true;
+                    if($this->range[ $this->mid_range - 1 ] < $this->num_pages - 1 && $i == $this->range[ $this->mid_range - 1 ]) {
+                        $this->return->pages->$i->dots = true;
                     }
                 } //end for
-                if($this->current_page < $this->num_pages And $this->total_items >= 10 And $this->items_per_page != "All" And $this->current_page > 0) {
+                if($this->current_page < $this->num_pages && $this->total_items >= 10 && $this->items_per_page != "All" && $this->current_page > 0) {
                     $this->return->next->status = true;
                     $this->return->next->url = $this->url;
                     $this->return->next->page = $this->current_page + 1;
@@ -172,55 +157,21 @@
                 // if($this->num_pages < 10)
             } else {
                 for ($i = 1; $i <= $this->num_pages; $i++) {
+                    $this->return->pages->$i = new \stdClass();
                     if($i == $this->current_page) {
-                        $this->return .= $this->twig->render('Paginate.twig', array("tag" => array(
-                            "1" => array(
-                                "params" => array(
-                                    "odd_even" => "odd",
-                                    "tagName"  =>"span",
-                                    "transparency" => 'true',
-                                    "instance" => $this->instance,
-                                    "class"    => "paginBut current",
-                                    "html"     => $i,
-                                )),
-                        )));
+                        $this->return->pages->$i->i = $i;
                     } else {
-                        $this->return .= $this->twig->render('Paginate.twig', array("tag" => array(
-                            "1" => array(
-                                "params" => array(
-                                    "odd_even" => "odd",
-                                    "tagName"  =>"span",
-                                    "transparency" => 'true',
-                                    "instance" => $this->instance,
-                                    "class"    => "paginBut pointer",
-                                    "html"     => $i,
-                                    "href"     => array(
-                                        "url"  => $this->url,
-                                        "ipp"  => $this->items_per_page,
-                                        "page" => $i
-                                    ))),
-                        )));
+                        $this->return->pages->$i->url = $this->url;
+                        $this->return->pages->$i->ipp = (int)$this->items_per_page;
+                        $this->return->pages->$i->i = $i;
+                        $this->return->pages->$i->title = $this->num_pages;
                     }
                 }
-
-                $this->return .= $this->twig->render('Paginate.twig', array("tag" => array(
-                    "1" => array(
-                        "params" => array(
-                            "odd_even" => "odd",
-                            "tagName"  =>"span",
-                            "transparency" => 'true',
-                            "instance" => $this->instance,
-                            "class"    => "paginBut active pointer",
-                            "html"     => "All",
-                            "href"     => array(
-                                "url"  => $this->url,
-                                "ipp"  => "All",
-                                "page" => "1"
-                            )
-                        )),
-                )));
+                $this->return->all->url =  $this->url;
+                $this->return->all->ipp = 'All';
+                $this->return->all->page = '1';
             }
-            $this->return = str_replace("&", "&amp;", $this->return);
+            //$this->return = str_replace("&", "&amp;", $this->return);
             $this->limit_start = ($this->current_page <= 0) ? 0 : ($this->current_page - 1) * $this->items_per_page;
             if($this->current_page <= 0) $this->items_per_page = 0;
             if($this->current_page == 0) {
@@ -229,13 +180,23 @@
                 $endPagePlus = 0;
             }
             $this->limit_end = ($this->items_per_page == "All") ? (int)$this->total_items :$endPagePlus + (int)$this->items_per_page;
+
+            print_r( $this->return);
+        }
+
+        public function createReturnObj() {
+            $this->return = new \stdClass();
+            $this->return->prev = new \stdClass();
+            $this->return->pages = new \stdClass();
+            $this->return->next = new \stdClass();
+            $this->return->all = new \stdClass();
         }
 
         public function display_items_per_page()
         {
             natsort($this->ipp_array);
 
-            return $this->twig->render('PaginateItems.twig', array(
+           /** return $this->twig->render('PaginateItems.twig', array(
                 "spanClass"   => "items",
                 "selectclass" => "itemsSelect",
                 "array"       => $this->ipp_array,
@@ -243,78 +204,56 @@
                 "href"        => array(
                     "url"  => "http://localhost/WebMillions/index.php&url=User",
                 )
-            ));
+            ));*/
+        }
+
+        public function setAjax() {
+            $this->items_per_page = $this->ipp_array[0];
+
+            if($this->items_per_page == "All") {
+                $this->num_pages = 1;
+            } else {
+                if(!is_numeric($this->items_per_page) || $this->items_per_page <= 0) $this->items_per_page = $this->ipp_array[0];
+                $this->num_pages = ceil($this->total_items / $this->items_per_page);
+            }
+        }
+
+        public function setGet(){
+            array_splice($_GET, 3);
+            if(isset($_GET["PaginateToGo"])) {
+                //  print_r($_GET["PaginateToGo"]);
+                $args[0] = "page=" . $_GET["page"];
+                $args[1] = "ipp=" . $_GET["ipp"];
+                $args[2] = "url=" . $_GET['url'];
+            } else {
+                $args = explode("&", $_SERVER["QUERY_STRING"]);
+            }
+            foreach ($args as $arg) {
+                $keyval = explode("=", $arg);
+                //   print_r($keyval);
+                if($keyval[0] != "page" && $keyval[0] != "ipp") $this->querystring .= "&" . $arg;
+            }
         }
 
         public function display_jump_menu()
         {
             $option = NULL;
-            for ($i = 1; $i <= $this->num_pages; $i++) {
-                $option .= ($i == $this->current_page) ? "<option value=\"$i\" selected>$i</option>\n" : "<option value=\"$i\">$i</option>\n";
-            }
-
-            return "<span class=\"paginate\">Page:</span><select class=\"paginate\" onchange=\"window.location='$_SERVER[PHP_SELF]?page='+this[this.selectedIndex].value+'&amp;ipp=$this->items_per_page$this->querystring';return false\">$option</select>\n";
+            return $this->renderView('display_jump_menu.twig', [
+                'num_pages' => $this->num_pages,
+                'url' => $this->url,
+                'items_per_page' => $this->items_per_page
+            ]);
         }
 
         public function  go_to_page()
         {
-            return $this->twig->render('Paginate.twig', array("tag" => array(
-                "2" => array(
-                    "params" => array(
-                        "odd_even" => "even",
-                        "tagName"  => "input",
-                        "class"       => "inputPage",
-                        "type"     => "number",
-                        "min"      => "1",
-                        "max"      => (int)$this->num_pages,
-                    )),
-                "3" => array(
-                    "params" => array(
-                        "odd_even" => "even",
-                        "tagName"  => "input",
-                        "id"       => "url",
-                        "type"     => "hidden",
-                        "value"    => $this->url
-                    )),
-                "4" => array(
-                    "params" => array(
-                        "odd_even" => "even",
-                        "tagName"  => "input",
-                        "class"       => "ipp",
-                        "type"     => "hidden",
-                        "value"    => $this->items_per_page
-                    )),
-                "1" => array(
-                    "params" => array(
-                        "odd_even" => "odd",
-                        "tagName"  =>"span",
-                        "transparency" => 'true',
-                        "instance" => $this->instance,
-                        "class"    => "paginateToGo paginate",
-                        "href"     => array(
-                            "url"  => $this->url,
-                            "ipp"  => (int)$this->items_per_page,
-                            "page" => $this->current_page
-                        ),
-                        "html"     => $this->lang['go'],
-                    )),
-            )));
+            return $this->renderView('go_to_page.twig', [
+                'max' => (int)$this->num_pages,
+                'url' => $this->url,
+                'items_per_page' => $this->items_per_page
+            ]);
         }
 
-        public function getLang()
-        {
-            $this->lang = $this->container->get('language')->getLang("paginator");
-        }
 
-        public function getJsScript()
-        {
-            return $this->twig->render("PaginatorJavaScript.twig", array(
-                "url" => "http://localhost/WebMillions/index.php&url=User",
-            ));
-        }
-
-        public function getCss()
-        {
-            return array("css/Paginate.css");
-        }
+        
     }
