9dbd5b26324245a2e5dced81a5de9a242a402b89
Norbi <nazinorbi@gmail.com>
2016-12-01 10:55:20 +0100

    add newPages() and createDots()  and firstLast() ok...

--- src/IndexBundle/Controller/StratiController.php
+++ src/IndexBundle/Controller/StratiController.php
@@ -29,9 +29,9 @@ class StratiController extends AbsBootstrap
     public function indexAction(Request $request) {
 
         $parameters = [
-            'totalItems' => 500,
-            'midRange' => 7,
-            'current_page' => 98,
+            'totalItems' => 120,
+            'midRange' => 9,
+            'current_page' => 2,
             'currentOfTotal' => true,
             'items_per_page' => 5,
             'defaultModelName' => 'model_1'
--- src/IndexBundle/Libs/Paginator.php
+++ src/IndexBundle/Libs/Paginator.php
@@ -2,9 +2,11 @@
     namespace IndexBundle\Libs;
     /**
      * PHP Pagination Class
-     * @auth|| admin@catchmyfame.com - http://www.catchmyfame.com
+     * @original auth|| admin@catchmyfame.com - http://www.catchmyfame.com
+     * rebild auth Nazimecki Norbert
      * @version 3.0.0
-     * @date February 6, 2014
+     * @date February 6, 2016
+     * @author
      * @copyright (c) admin@catchmyfame.com (www.catchmyfame.com)
      * @license CC Attribution-ShareAlike 3.0 Unp||ted (CC BY-SA 3.0) - http://creativecommons.||g/licenses/by-sa/3.0/
      */
@@ -14,8 +16,12 @@
 
     class Paginator
     {
-
+        /**
+         * This is the current page
+         * @var integer
+         * */
         private $current_page;
+
         private $items_per_page;
         private $mid_range;
         private $ipp_array;
@@ -23,8 +29,10 @@
         private $defaultModelName;
         private $url;
 
-        private $limit_end;
-        private $limit_start;
+        /**
+         * Total pages number in query
+         * @var integer
+         */
         private $numPages;
         private $limit;
         private $querystring;
@@ -37,7 +45,8 @@
         private $currentOfTotal = true;
         private $dotsNumber = [];
         private $halfMidRange;
-        private $befAfterNum = 2;
+        private $befAfterNum = 1;
+        private $createDots = true;
 
         private $return;
         protected $container;
@@ -67,28 +76,10 @@
             foreach ($parameters as $key => $parameter) {
                 switch ($key) {
                     case 'totalItems';
-                        if( $parameters['items_per_page'] == 'All') {
-                            $this->numPages = 1;
-                            $this->current_page = 1;
-                            $this->total_items = $parameter;
-                            $this->items_per_page = 'All';
-                        }
-                        elseif(is_int($parameter) ) {
-                            $this->total_items = $parameter;
-                            $this->numPages = ceil($this->total_items / $this->items_per_page);
-                        } else {
-                            $this->total_items = $this->getEntityManager()
-                                ->createQuery('
-                                    SELECT count(*) as total
-                                    FROM '.$parameters['tableName'].': tableName
-                                    WHERE tabbleName = :tableName
-                                    ')->setParameter('tableName', $parameters['tableName']);
-                            $this->numPages = ceil($this->total_items / $this->items_per_page);
-                        }
+                        $this->paramTotalItems($parameters, $parameter);
                     break;
                     case 'midRange':
-                        $this->mid_range = $parameter;
-                        $this->halfMidRange = ($this->mid_range-1)/2;
+                        $this->paramMidRange($parameter);
                     break;
                     case 'ippArray':
                         $this->ipp_array = $parameter;
@@ -117,6 +108,38 @@
             return call_user_func([$this, $this->defaultModelName]);
         }
 
+        public function paramTotalItems($parameters, $parameter) {
+            if( $parameters['items_per_page'] == 'All') {
+                $this->numPages = 1;
+                $this->current_page = 1;
+                $this->total_items = $parameter;
+                $this->items_per_page = 'All';
+            }
+            elseif(is_int($parameter) ) {
+                $this->total_items = $parameter;
+                $this->numPages = ceil($this->total_items / $this->items_per_page);
+            } else {
+                $this->total_items = $this->getEntityManager()
+                    ->createQuery('
+                                    SELECT count(*) as total
+                                    FROM '.$parameters['tableName'].': tableName
+                                    WHERE tabbleName = :tableName
+                                    ')->setParameter('tableName', $parameters['tableName']);
+                $this->numPages = ceil($this->total_items / $this->items_per_page);
+            }
+        }
+
+        public function paramMidRange($parameter) {
+            if($parameter > $this->numPages) {
+                $this->mid_range = $this->numPages;
+                $this->createDots = false;
+                $this->first = false;
+            } else {
+                $this->mid_range = $parameter;
+                $this->halfMidRange = ($this->mid_range-1)/2;
+            }
+        }
+
         public function model_1()
         {
             if($this->total_items <= 0) {
@@ -134,20 +157,20 @@
                 $this->return->all->status = true;
                 $this->return->all->ipp = $this->ipp_array;
             }
-            else if($this->numPages > 10) {
+            else {
                 $this->newPages();
-                $this->createDots();
+                ($this->createDots) ? $this->createDots(): false;
+                $this->controllerStr();
+                $this->go_to_page();
+                $this->all();
+                $this->ipp();
 
                 for ($i = 0; $i <= count($this->range)-1; $i++) {
                     $n = $this->range[$i] ?? $this->range[$i];
-                    $dotsA = $this->range[$i] > 2 && $n < $this->current_page - $this->halfMidRange ? true : false;
-                    $dotsB = ($n == $this->current_page + $this->halfMidRange + 1 && $n < $this->numPages-2) == -1 ? true : false;
 
-                    if( $dotsA ||$dotsB ) {
+                    if( in_array($n, $this->dotsNumber) ) {
                         $this->return->pages->$n = new \stdClass();
                         $this->return->pages->$n->dots = true;
-                        $this->dotsNumber[] = $this->range[$i];
-
                     } else {
                         $this->return->pages->$n = new \stdClass();
                         $this->return->pages->$n->i = $this->range[$i];
@@ -158,44 +181,16 @@
                             $this->return->pages->$n->class = 'paginate active';
                         }
                     }
-                } //end for
-                // if($this->numPages < 10)
-            } else {
-                for ($i = 1; $i <= $this->numPages; $i++) {
-                    $this->return->pages->$i = new \stdClass();
-                    $this->return->pages->$i->i = $i;
-
-                    if($i == $this->current_page && $this->items_per_page != "All") {
-                        $this->return->pages->$i->class = 'current';
-                    } else {$this->return->pages->$i->class = 'paginate active';
-                    }
                 }
-                $this->return->all->ipp = 'All';
             }
-            $this->limit_start = ($this->current_page <= 0) ? 0 : ($this->current_page - 1) * $this->items_per_page;
-            if($this->current_page <= 0) $this->items_per_page = 0;
-            if($this->current_page == 0) {
-                $endPagePlus = 2;
-            } else {
-                $endPagePlus = 0;
-            }
-            $this->limit_end = ($this->items_per_page == "All") ? (int)$this->total_items :$endPagePlus + (int)$this->items_per_page;
-
-
-            $this->controllerStr();
-            $this>$this->go_to_page();
-            $this->all();
-            $this->ipp();
-
-           // $this->newPages();
-            //$this->firsLast();
-            //print_r($this->return);
+           // print_r($this->return);
+            $this->firsLast();
             return  $this->return;
         }
 
         public function newPages() {
 
-            if($this->current_page <= $this->halfMidRange +1) {
+            if($this->current_page <= $this->halfMidRange +2) {
                 $this->start_range = 1;
                 $this->end_range = $this->mid_range;
             }
@@ -205,7 +200,7 @@
                 $this->end_range = $this->current_page + $this->halfMidRange;
             }
 
-            if($this->current_page >= $this->numPages - ($this->halfMidRange+2)) {
+            if($this->current_page >= $this->numPages - ($this->halfMidRange+1)) {
                 $this->start_range = $this->numPages - ($this->mid_range-1);
                 $this->end_range = $this->numPages;
             }
@@ -217,24 +212,66 @@
             $beforeAfterNumber = true;
 
             if($beforeAfterNumber) {
-                if($this->current_page - $this->halfMidRange > $this->befAfterNum ) {
-                    array_unshift($this->range, $this->current_page - $this->halfMidRange -1);
+                /**
+                 * Az elején van a dots;
+                 */
+                if($this->current_page >= $this->numPages - $this->halfMidRange ) {
                     if($this->befAfterNum > 1) {
                         for($i = $this->befAfterNum; $i >= 1; $i--) {
                             array_unshift($this->range, $i);
+                            if($i == $this->befAfterNum ) {
+                                $this->dotsNumber[] = $i;
+                            }
                         }
                     } else {
+                        array_unshift($this->range, 2);
                         array_unshift($this->range, 1);
+                        $this->dotsNumber[] = 2;
                     }
                 }
-                if($this->current_page + $this->halfMidRange + 1 < $this->numPages-abs($this->befAfterNum) ) {
-                    array_push($this->range, $this->current_page + ($this->mid_range-1)/2 + 1);
+                /**
+                 * Az végén van a dots;
+                 */
+                if($this->current_page <= $this->halfMidRange + 2) {
+                  //  array_push($this->range, $this->current_page + ($this->mid_range-1)/2 + 1);
                     if($this->befAfterNum > 1) {
                         for($i = $this->numPages - $this->befAfterNum+1; $i <= $this->numPages; $i++) {
                             array_push($this->range, $i);
+                            if($i == $this->numPages - $this->befAfterNum+1) {
+                                $this->dotsNumber[] = $i;
+                            }
                         }
                     } else {
-                        array_push($this->range, $this->range);
+                        array_push($this->range, $this->numPages-1);
+                        array_push($this->range, $this->numPages);
+                        $this->dotsNumber[] = $this->numPages-1;
+                    }
+                }
+                /**
+                 * Az elején és a végén van a dots;
+                 */
+                if($this->current_page-$this->halfMidRange > $this->befAfterNum+1
+                    && $this->current_page+$this->halfMidRange < $this->numPages-($this->befAfterNum)) {
+                    if($this->befAfterNum > 1) {
+                        for($i = $this->befAfterNum+1; $i >= 1; $i--) {
+                            array_unshift($this->range, $i);
+                            if($i == $this->befAfterNum ) {
+                                $this->dotsNumber[] = $this->befAfterNum+1;
+                            }
+                        }
+                        for($i = $this->numPages - $this->befAfterNum; $i <= $this->numPages; $i++) {
+                            array_push($this->range, $i);
+                            if($i == $this->numPages - $this->befAfterNum+1) {
+                                $this->dotsNumber[] = $this->numPages - $this->befAfterNum;
+                            }
+                        }
+                    } else {
+                        array_push($this->range, $this->numPages-1);
+                        array_push($this->range, $this->numPages);
+                        array_unshift($this->range, 2);
+                        array_unshift($this->range, 1);
+                        $this->dotsNumber[] = 2;
+                        $this->dotsNumber[] = $this->numPages-1;
                     }
                 }
             }
@@ -273,43 +310,99 @@
             $lastClass = 'active';
 
             if($this->first) {
-                if($this->return->pages->{1}->i == 1 && $this->current_page - ($this->mid_range-1/2) > 1){
-                    unset($this->return->pages->{1});
+                /**
+                 * Az elején van a dots;
+                 */
+                if($this->current_page >= $this->numPages - $this->halfMidRange){
+                    if($this->befAfterNum > 1) {
+                        for($i = $this->befAfterNum; $i >= 1; $i--) {
+                            unset($this->return->pages->{$i});
+                        }
+                    } else {
+                        unset($this->return->pages->{1});
+                    }
                 }
-                if($this->current_page + ($this->mid_range-1)/2 <= $this->numPages) {
-                    unset($this->return->pages->{$this->numPages});
+                /**
+                 * Az végén van a dots;
+                 */
+                if($this->current_page <= $this->halfMidRange + 2) {
+                    if($this->befAfterNum > 1) {
+                        for($i = $this->numPages - $this->befAfterNum; $i <= $this->numPages; $i++) {
+                            unset($this->return->pages->{$i});
+                        }
+                    } else {
+                        unset($this->return->pages->{$this->numPages});
+                    }
                 }
-                unset($this->return->pages->{$this->dotsNumber[0]});
+                /**
+                 * Az elején és a végén van a dots;
+                 */
+                if($this->current_page-$this->halfMidRange > $this->befAfterNum+1
+                    && $this->current_page+$this->halfMidRange < $this->numPages-($this->befAfterNum) ) {
+                    if($this->befAfterNum > 1) {
+                        for($i = $this->befAfterNum; $i >= 1; $i--) {
+                            unset($this->return->pages->{$i});
+                        }
+                        for($i = $this->numPages - $this->befAfterNum; $i <= $this->numPages; $i++) {
+                            unset($this->return->pages->{$i});
+                        }
+                    } else {
+                        unset($this->return->pages->{1});
+                        unset($this->return->pages->{$this->numPages});
+                    }
+                }
+                /**
+                 * unset dots
+                 */
+                if(isset($this->dotsNumber[0])) {
+                    unset($this->return->pages->{$this->dotsNumber[0]});
+                }
                 if(isset($this->dotsNumber[1]) ){
                     unset($this->return->pages->{$this->dotsNumber[1]});
                 }
-            }
-            if($this->current_page == 1) {
-                $firstClass = 'inactive';
-            }
-            if($this->current_page ==  $this->numPages) {
-                $lastClass = 'inactive';
-            }
-            //if($this->current_page != $this->numPages) {
-                $this->return->firstLast = new \stdClass();
-                $this->return->firstLast->firstClass = $firstClass;
-                $this->return->firstLast->lastClass = $lastClass;
-                switch ($param) {
-                    case 'FontAwesome':
-                        $this->return->firstLast->status = 'FontAwesome';
-                        $this->return->firstLast->name = 'angle-double';
-                        break;
-                    case 'string':
-                        $this->return->firstLast->status = 'string';
-                        break;
-                    case 'arrowString':
-                        $this->return->firstLast->status = 'arrowString';
-                        $this->return->firstLast->name = 'angle-double';
-                        break;
-                }
-                $this->getClass();
-            //}
 
+                if($this->current_page == 1) {
+                    $firstClass = 'inactive';
+                }
+                if($this->current_page ==  $this->numPages) {
+                    $lastClass = 'inactive';
+                }
+                if(!$this->first) {
+                    $this->return->firstLast = new \stdClass();
+                    $this->return->firstLast->firstClass = $firstClass;
+                    $this->return->firstLast->lastClass = $lastClass;
+                    switch ($param) {
+                        case 'FontAwesome':
+                            $this->return->firstLast->status = 'FontAwesome';
+                            $this->return->firstLast->name = 'angle-double';
+                            break;
+                        case 'string':
+                            $this->return->firstLast->status = 'string';
+                            break;
+                        case 'arrowString':
+                            $this->return->firstLast->status = 'arrowString';
+                            $this->return->firstLast->name = 'angle-double';
+                            break;
+                    }
+                    $this->getClass();
+                }
+
+            }
+        }
+
+        public function getClass() {
+            if($this->current_page == $this->numPages) {
+                $this->return->firstLast->classLast = 'inactive';
+                $this->return->firstLast->classFirst = 'active';
+            } else if($this->current_page == 1) {
+                $this->return->firstLast->classLast = 'active';
+                $this->return->firstLast->classFirst = 'inactive';
+            }
+        }
+
+        public function go_to_page()
+        {
+            $this->return->goto = new \stdClass();
         }
 
         public function all() {
@@ -348,18 +441,4 @@
             }
         }
 
-        public function go_to_page()
-        {
-            $this->return->goto = new \stdClass();
-        }
-
-        public function getClass() {
-            if($this->current_page == $this->numPages) {
-                $this->return->firstLast->classLast = 'inactive';
-                $this->return->firstLast->classFirst = 'active';
-            } else if($this->current_page == 1) {
-                $this->return->firstLast->classLast = 'active';
-                $this->return->firstLast->classFirst = 'inactive';
-            }
-        }
     }
