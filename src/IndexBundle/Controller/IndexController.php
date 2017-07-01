4ad3299c7d957291f5d39c15afaa46091a4ef730
nazi <nazinorbi@gmail.com>
2017-07-01 20:34:07 +0200

    add new function index (update sql table)

--- src/IndexBundle/Controller/IndexController.php
+++ src/IndexBundle/Controller/IndexController.php
@@ -10,12 +10,11 @@ namespace IndexBundle\Controller;
 
 use IndexBundle\_Interface\HeadCreaturInterface;
 use IndexBundle\Libs\AbsBootstrap;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
 * Controller used to manage blog contents in the public part of the site.
@@ -26,27 +25,52 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class IndexController extends AbsBootstrap implements HeadCreaturInterface {
 
-    public function run() {
-        return $this->bootstrapRun($center = null);
+    private $data;
+    private $text;
+    private $sqlResponse = null;
+
+    public function run($center) {
+        return $this->bootstrapRun($center);
     }
     /**
      * @Route("/", name="index")
      * @Method({"GET", "POST"})
      */
-    public function indexAction()
+    public function indexAction($data = null, $ajax = false, Request $request)
     {
-        return $this->run();
+        $this->data = $data;
+        $this->request = $this->data->url ?? $request->get('_route');
+        $this->text = $this->getDoctrine()
+            ->getRepository('IndexBundle:Index')
+            ->getText();
+
+        $visiable_ = false;
+
+        if(isset($_SESSION['user']) && $_SESSION['user']->userRank == 'owen') {
+            $visiable_ = true;
+        }
+
+        if($ajax) {
+            $this->updateIndexData();
+        }
+        if($ajax && $this->sqlResponse) {
+            return new Response(json_encode(['sqlResponse' => true, 'text' => $this->text]));
+        }
+        elseif($this->sqlResponse !== true && $ajax) {
+            return new JsonResponse(['sqlResponse', $this->sqlResponse]);
+        }
+        else {
+            return $this->run($this->renderView('index.twig', [
+                'textEdior' => $this->get('translator')->trans('TEXT_EDITOR'),
+                'visiable' => $visiable_,
+                'text'  => $this->text->szoveg
+            ]));
+        }
     }
 
+    private function updateIndexData() {
+        $em = $this->getDoctrine()->getManager();
 
-
-    public function showAction()
-    {
-       /* $repository = $this->getDoctrine()
-            ->getRepository('IndexBundle:Header');
-
-        $repo = $repository->findAll();*/
+        $this->sqlResponse = $em->getRepository('IndexBundle:Index')->updateIndex($this->data->indexText);
     }
-
-
 }
